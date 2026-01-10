5856e9582acfa5b2b3ef922b104108f779c733a0
forge-team <forge.code.2025@gmail.com>
2026-01-10 21:40:01 +0100

    fixed file order in Setup.f90

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-ifort -qopenmp -o test LapackRoutines.f90 Geometry.f90 Setup.f90 TightBinding.f90 HartreeFock.f90 Main.f90 -qmkl -lpthread -lm
+ifort -qopenmp -o test LapackRoutines.f90 Setup.f90 Geometry.f90 TightBinding.f90 HartreeFock.f90 Main.f90 -qmkl -lpthread -lm
